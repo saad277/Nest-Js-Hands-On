c551d6c4a87eee0810f52acfaaf4c153fda4573d
Saad Sheikh <saadsheikh27@gmail.com>
2021-01-09 16:03:12 +0500

    user auth module

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,13 @@
+import {Repository,EntityRepository} from "typeorm"
+import {User} from "./user.entity"
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User>{
+
+
+    async singUp(){
+
+            
+    }
+
+}
